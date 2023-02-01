bef47e25aea99386f13b34fdd669c6b8bfa9c7c4
hasan <kejoramedia@gmail.com>
2023-02-01 14:32:37 +0700

    Aplikasi Deteksi Gempa Terkini Modularisasi Package

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 def ekstraksi_data():
     """
-    Tanggal: 30 Januari 2023
+    Waktu: 30 Januari 2023
     Waktu: 13: 24:19 WIB
     Magnitudo: 3.2
     Kedalaman: 3 km
@@ -19,20 +19,29 @@ def ekstraksi_data():
         return None
 
     if content.status_code == 200:
-        print(content.status_code)
-        #soup = BeautifulSoup(content)
-        #print(soup.prettify())
+        soup = BeautifulSoup(content.text, 'html.parser')
 
-        hasil = dict()
-        hasil['Tanggal'] = '30 Januari 2023'
-        hasil['Waktu'] = '13: 24:19 WIB'
-        hasil['Magnitudo'] = '3.2'
-        hasil['Kedalaman'] = '3 km'
-        hasil['Lokasi'] = {'LS': 3.33, 'BT': 128.35}
-        hasil['Pusat Gempa'] = 'Pusat gempa berada di darat 1 km Utara Kairatu'
-        hasil['dirasakan'] = "Dirasakan (Skala MM1): III kairatu"
+        result = soup.find('div',{'class':'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        result = result.findChildren('li')
 
+        tanggal_waktu = result[0].text
+        tanggal = tanggal_waktu.split(',')[0]
+        waktu = tanggal_waktu.split(',')[1]
+        magnitude =  result[1].text
+        kedalaman =  result[2].text
+        lokasi =  result[3].text
+        pusat_gempa =  result[4].text
+        dirasakan =  result[5].text
+
+        hasil = {'Tanggal': tanggal,
+                 'Waktu': waktu,
+                 'Magnitudo': magnitude,
+                 'Kedalaman': kedalaman,
+                 'Lokasi': lokasi,
+                 'Pusat Gempa': pusat_gempa,
+                 'Dirasakan': dirasakan}
         return hasil
+
     else:
         return None
 
@@ -47,6 +56,8 @@ def tampilkan_data(result):
     print(f"Waktu {result['Waktu']}")
     print(f"Magnitudo {result['Magnitudo']}")
     print(f"Kedalaman {result['Kedalaman']}")
-    print(f"Lokasi: LS= {result['Lokasi']['LS']}, BT= {result['Lokasi']['BT']}")
-    print(f"Pusat Gempa {result['Pusat Gempa']}")
-    print(f"dirasakan {result['dirasakan']}")
+    print(f"Lokasi {result['Lokasi']}")
+    print(f"{result['Pusat Gempa']}")
+    print(f"{result['Dirasakan']}")
+
+
